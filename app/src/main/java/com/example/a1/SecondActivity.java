19c9bc59d07fdffdea99249da7830e7d3a2688a8
Ahmed A <163431268+ahmed-almouna@users.noreply.github.com>
2025-02-01 16:05:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a1/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.a1;
+import android.os.Bundle;
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+
+public class SecondActivity extends ComponentActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
