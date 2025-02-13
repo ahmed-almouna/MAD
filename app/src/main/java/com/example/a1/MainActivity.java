9eb654d521702f37a482b6f5d8bac545813f73d2
CoombsCarter <120283653+CoombsCarter@users.noreply.github.com>
2025-02-13 16:30:33 -0500

    Updated MainActivity, added BookingActivity & DestinationActivity, removed unused activities

--- app/src/main/java/com/example/a1/MainActivity.java
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.a1;
-import android.os.Bundle;
-import androidx.activity.ComponentActivity;
-import androidx.annotation.Nullable;
 
-public class MainActivity extends ComponentActivity
-{
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.ComponentActivity;
+
+public class MainActivity extends ComponentActivity {
+    private Button addTripButton;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        // Initialize add trip button
+        addTripButton = findViewById(R.id.new_trip_btn);
+
+        // Add Trip Button Click Listener
+        addTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DestinationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
